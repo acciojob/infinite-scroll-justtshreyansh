3847f0a2689ae999b1953c356b1ff2536bb0907c
Shreyansh <149056401+justtshreyansh@users.noreply.github.com>
2025-06-20 15:43:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here!
 
+const main = document.querySelector('main');
+const ol = document.querySelector('#infi-list');
+
+let count = 1;
+function addItems(n){
+  for(let i=0;i<n;i++){
+    const ele = document.createElement('li');
+    ele.textContent = `Item${count++}`;
+    
+    ol.appendChild(ele);
+  }
+}
+
+addItems(10);
+
+main.addEventListener('scroll', () => {
+    if (main.scrollTop + main.clientHeight >= main.scrollHeight) {
+      addItems(2); 
+    }
+  });
